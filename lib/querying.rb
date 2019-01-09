8dcbe43f63eb450f9d9377e1147c12f628b43562
Molly McCarron <mccarronmollye@gmail.com>
2019-01-09 20:24:25 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -13,7 +13,7 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT characters.species, COUNT *
+  "SELECT characters.species, COUNT (*)
   FROM characters
   GROUP BY characters"
 end
