f97d951c43b13b62914a6e74bfe7a64cb81a9751
Molly McCarron <mccarronmollye@gmail.com>
2019-01-09 20:24:08 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -13,7 +13,9 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT "
+  "SELECT characters.species, COUNT *
+  FROM characters
+  GROUP BY characters"
 end
 
 def select_name_and_series_subgenres_of_authors
