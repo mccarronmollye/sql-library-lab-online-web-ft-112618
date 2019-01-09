d4c31fd5448bf37177713d2e02785005724a2986
Molly McCarron <mccarronmollye@gmail.com>
2019-01-09 20:48:17 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -31,7 +31,8 @@ end
 def select_series_title_with_most_human_characters
  "SELECT series.title
   FROM series
-  JOIN characters on series.id = characters.series_id"
+  JOIN characters on series.id = characters.series_id
+  LIMIT 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
