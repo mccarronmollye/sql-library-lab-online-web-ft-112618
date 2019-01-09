a5421f3eea1c118fbedd8743ea9dde1f0fce23b3
Molly McCarron <mccarronmollye@gmail.com>
2019-01-09 21:13:41 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -43,5 +43,6 @@ def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.name, count(*)
    FROM characters
    JOIN character_books ON characters.id = character_books.character_id
-   GROUP BY characters.id"
+   GROUP BY characters.id
+   ORDER BY characters.id"
 end
