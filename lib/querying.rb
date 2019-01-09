cccee08524c679e60807ea6f2bb7aef9fadf2bfd
Molly McCarron <mccarronmollye@gmail.com>
2019-01-09 21:07:25 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -40,6 +40,6 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT characters.name, COUNT(*)
+  "SELECT characters.name, as number_of_books_they_are_in
    FROM characters"
 end
