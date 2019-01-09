13beb017a3156b303dbea6f0e45a4ae71677547d
Molly McCarron <mccarronmollye@gmail.com>
2019-01-09 20:53:43 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -34,7 +34,7 @@ def select_series_title_with_most_human_characters
   JOIN characters on series.id = characters.series_id
   GROUP BY characters.species
   HAVING COUNT(*)
-  ORDER BY characters.species
+  ORDER BY characters.species DESC
   LIMIT 1"
 end
 
